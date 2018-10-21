fbc51940f595e293cf4b3e1745b39d63c858c1b2
Conner Ardman <ardmanc@cs.washington.edu>
2018-10-21 02:49:01 -0700

    API utils

--- webapp/src/utils/apiUtils.js
+++ webapp/src/utils/apiUtils.js
@@ -3,7 +3,7 @@
  * getBookInformation - fetches https://openlibrary.org/api/
  *
  * @param  {number[]} isbns isbn numbers
- * @return {Object}
+ * @return {Promise}
  */
 export function getBookInformation(isbns) {
    const apiEndpoint = 'https://openlibrary.org/api/books?bibkeys=';
@@ -18,7 +18,43 @@ export function getBookInformation(isbns) {
    return fetch(apiEndpoint + isbnParam + apiExtraParams).then(checkStatus).then(JSON.parse).catch(console.log);
 }
 
-export function checkStatus(response) {
+// Returns a promise of books JSON
+// books_required: [book1, book2, etc]
+// books_owned: [book1, book2, etc]
+export function getBooks(email) {
+   const url = 'http://localhost:3000/books?email=';
+   return fetch(url + email, {
+      headers: {
+         'Content-Type': 'application/x-www-form-urlencoded'
+      }
+   }).then(checkStatus).then(JSON.parse).catch(console.log);
+}
+
+export function postBookOwned(email, isbn) {
+   postBook(email, isbn, 'book_owned');
+}
+
+export function postBookRequired(email, isbn) {
+   postBook(email, isbn, 'book_required');
+}
+
+function postBook(email, isbn, endpoint) {
+   const data = {
+      email: email,
+      isbn: isbn
+   };
+
+   const url = 'http://localhost:3000/' + endpoint;
+   fetch(url, {
+               method: "POST",
+               body: JSON.stringify(data),
+               headers: {
+                  'Content-Type': 'application/json'
+               }
+            }).catch(console.log);
+}
+
+function checkStatus(response) {
    if (response.status >= 200 && response.status < 300) {
       return response.text();
    } else {
