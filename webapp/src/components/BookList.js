9d79d2ee160d73deb3cadcc6b75e0ddb1c0fdbe1
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2018-10-21 01:28:55 -0700

    compiles but dont work lol

---
+++ webapp/src/components/BookList.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import styles from './BookList.css'
+import { getBookInformation } from '../utils/apiUtils'
+
+export default class BookList extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      bookData: [],
+      isbns: []
+    };
+  
+  }
+
+  componentWillUpdate() {
+    var self = this;
+    getBookInformation(this.state.isbns).then(
+      function(data){
+        self.setState({
+          bookData: Object.values(data)
+        });
+      }
+    );
+  }
+
+
+
+  render() {   
+    if (typeof this.state.bookData === "undefined" || this.state.bookData.length == 0) {
+      return (
+        <div>
+          <p>Add a book!</p>
+        </div>
+      )
+    } else {        
+      return (
+        <div>
+            <div className={styles.listtitle}>{this.props.title}</div>
+            <div>
+            {
+                this.state.bookData.map((item) => 
+                    <div style={styles.bookitem}>
+                        <img className={styles.bookimage} src={item.cover.medium} />
+                        <div style={styles.booktext}>
+                            <div style={styles.booktitle}>{item.title}</div>
+                            <div style={styles.bookauthors}>
+                                {
+                                    typeof item.authors === "undefined" || item.authors.length === 0 ? "Author(s) unavailable" :
+                                    item.autxhors.map((x)=>(x.name)).join(", ")
+                                }
+                            </div>
+                        </div>
+                    </div>)
+            }
+            </div>
+      </div>
+      )}
+
+  }
+}
---
+++ webapp/src/utils/apiUtils.js
@@ -0,0 +1,27 @@
+
+/**
+ * getBookInformation - fetches https://openlibrary.org/api/
+ *
+ * @param  {number[]} isbns isbn numbers
+ * @return {Object}
+ */
+export function getBookInformation(isbns) {
+   const apiEndpoint = 'https://openlibrary.org/api/books?bibkeys=';
+   const apiExtraParams = '&format=json&jscmd=data'
+   let isbnParam = '';
+   if (typeof isbns === "undefined") {
+       isbns = [];
+   }
+   isbns.forEach(isbn => {
+      isbnParam += 'ISBN:' + isbn + ',';
+   });
+   return fetch(apiEndpoint + isbnParam + apiExtraParams).then(checkStatus).then(JSON.parse).catch(console.log);
+}
+
+export function checkStatus(response) {
+   if (response.status >= 200 && response.status < 300) {
+      return response.text();
+   } else {
+      return Promise.reject(new Error(response.status+": "+response.statusText));
+   }
+}
