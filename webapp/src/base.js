e22580ffeb4a53cb23a6bff3758981d74a18b849
Conner Ardman <ardmanc@cs.washington.edu>
2018-10-21 00:27:55 -0700

    Create webapp

---
+++ webapp/src/base.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+import config from "./apiKeys";
+
+const app = firebase.initializeApp({
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
+  databaseURL: config.databaseURL,
+  projectId: config.projectId,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId
+});
+
+export default app;
